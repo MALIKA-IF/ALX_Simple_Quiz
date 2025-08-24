6a5b406adf5b42ad6a3e952f01326b660c961b08
Malika Edaif <edmalika957@gmail.com>
2025-08-24 21:26:10 +0100

    implement quiz.js file

--- quiz.js
+++ quiz.js
@@ -2,19 +2,21 @@ function checkAnswer() {
     
     const correctAnswer="4";
 
-    let userAnswer =document.querySelector('quiz').value();
+    let userAnswer =document.querySelector('input[name="quiz"]:checked').value;
 
     let feedback = document.getElementById('feedback');
     
-    const answer =document.getElementById('submit-answer');
 
-    if (correctAnswer=userAnswer){
 
-        feedback.textContent('Correct! Well done.');
+    if (correctAnswer === userAnswer){
+
+        feedback.textContent='Correct! Well done.';
     }else
     {
-        feedback.textContent("That's incorrect. Try again!");
+        feedback.textContent="That's incorrect. Try again!";
     }  
 }
 
-answer.addEventListener('submit',checkAnswer);
+const answer =document.getElementById('submit-answer');
+
+answer.addEventListener('click',checkAnswer);
